cc43bdc2fe6586f92e2389cd5287c2e47f38a16f
Jonathan Samines <jn.samines@gmail.com>
2015-10-13 23:52:04 -0600

    Added header styles and error redirection handler.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,7 @@ module.exports = function IndexController(router) {
   const application = {
     year: (new Date()).getFullYear()
   };
+
   router.get('/', function handleIndex(req, res) {
     res.render('index.html', application);
   });
