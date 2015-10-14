33518dfb8b5ca00d5958c68f5d50f9b44e120098
Jonathan Samines <jn.samines@gmail.com>
2015-10-13 22:59:18 -0600

    Created helper templates.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,13 @@
 module.exports = function IndexController(router) {
+  const application = {
+    year: (new Date()).getFullYear()
+  };
   router.get('/', function handleIndex(req, res) {
-    res.render('index.html');
+    res.render('index.html', application);
+  });
+
+  router.get('/home', function handleHome(req, res) {
+    res.render('index.html', application);
   });
 
   return ['/', router];
