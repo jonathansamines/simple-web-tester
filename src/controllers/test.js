fd14db83f6e4ae811db2632e078674039503ada0
Jonathan Samines <jn.samines@gmail.com>
2015-10-17 07:10:15 -0600

    Fixed alert for test terms acceptance.

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -7,14 +7,44 @@ module.exports = function TestController(router) {
     return testService
       .getFullTest(req.params.testId)
       .then(function responseWithTest(test) {
+        req.session.currentTest = test;
         res.render('test/index.html', {
-          test: test
+          test: test,
+          validation: req.flash('validation')[0]
         });
-      });
+      })
+      .then(null, console.log);
   });
 
-  router.get('/:testId/questions/:questionId', protect, function handleQuestionReqest(req, res) {
-    res.render('test/question.html');
+  router.post('/:testId', protect, function startTest(req, res) {
+    // validate if the user has accepted the rules
+    if (!req.body.acceptedRules) {
+      req.flash('validation', 'Para iniciar la prueba, es obligatorio aceptar los términos del test.');
+      return res.redirect(`/tests/${req.params.testId}`);
+    }
+
+    // start test
+    req.session.lastQuestion = 0;
+    res.redirect(`/tests/${req.params.testId}/questions/next`);
+  });
+
+  router.get('/:testId/questions/next', protect, function handleQuestionReqest(req, res) {
+    const lastQuestion = req.session.lastQuestion;
+    res.render('test/question.html', {
+      question: req.session.currentTest.questions[lastQuestion],
+      test: req.session.currentTest,
+      questionIndex: lastQuestion
+    });
+  });
+
+  router.post('/:testId/questions/next', protect, function requestNextQuestion(req, res) {
+    const lastQuestion = ++req.session.lastQuestion;
+
+    if (lastQuestion >= req.session.currentTest.questions.length) {
+      return res.redirect(`/tests/${req.params.testId}/result`);
+    }
+
+    return res.redirect(`/tests/${req.params.testId}/questions/next`);
   });
 
   router.get('/:testId/result', protect, function handleTestResult(req, res) {
